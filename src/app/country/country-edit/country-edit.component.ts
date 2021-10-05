c06edfd19b50ea16acd894cc36800b3cf3f8bc3a
GediminasGricius <g.gricius@ituostas.lt>
2021-10-05 20:24:14 +0300

    update

--- src/app/country/country-edit/country-edit.component.ts
+++ src/app/country/country-edit/country-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataService } from './../../data.service';
 
 @Component({
   selector: 'app-country-edit',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./country-edit.component.css']
 })
 export class CountryEditComponent implements OnInit {
-
-  constructor() { }
+  country;
+  world;
+  id;
+  constructor(private route:ActivatedRoute, private dataService:DataService, private router:Router) { }
 
   ngOnInit(): void {
+    this.world=this.dataService.world;
+    this.id=this.route.snapshot.params['id'];
+    this.country=this.world[this.id];
+  }
+
+  update(code, region,surfaceArea, year, population, lifeExpectancy){
+    this.country.Code=code.value;
+    this.country.Region=region.value;
+    this.country.SurfaceArea=surfaceArea.value;
+    this.country.IndepYear=year.value;
+    this.country.Population=population.value;
+    this.country.LifeExpectancy=lifeExpectancy.value;
+    this.router.navigate(['/country',this.id]);
+
   }
 
 }
--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -10,20 +10,21 @@ import { DataService } from '../data.service';
 export class CountryComponent implements OnInit {
   country;
   world;
+  id;
   constructor(private route:ActivatedRoute, private dataService:DataService) { }
 
   ngOnInit(): void {
     this.world=this.dataService.world;
 
-    let id=this.route.snapshot.params['id'];
-    this.country=this.world[id];
-    console.log(id);
+    this.id=this.route.snapshot.params['id'];
+    this.country=this.world[this.id];
+
 
     this.route.params.subscribe((params:Params)=>{
-      id=params['id'];
-      this.country=this.world[id];
+      this.id=params['id'];
+      this.country=this.world[this.id];
     });
 
   }
-
+  
 }
